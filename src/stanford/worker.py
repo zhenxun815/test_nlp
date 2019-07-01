82f5422a0d776c2f55f48450d4ba34f60f60d16b
zhenxun815 <guoyiheng89@gmail.com>
2019-07-01 19:01:40 +0800

    add stanford libs

---
+++ src/stanford/worker.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# @Description: 
+# @File: worker.py
+# @Project: test_nlp
+# @Author: Yiheng
+# @Email: GuoYiheng89@gmail.com
+# @Time: 6/27/2019 16:26
+
+import re
+import string
+
+from nltk.tokenize.stanford_segmenter import StanfordSegmenter
+from os import path
+
+
+class StanfordWorker:
+
+    def __init__(self):
+        file_path = path.realpath(__file__)
+        dir_path = path.dirname(file_path)
+        self.path_to_jar = path.join(dir_path, 'stanford-segmenter-3.9.2.jar')
+        self.path_to_model = path.join(dir_path, 'data/ctb.gz')  # pku.gz
+        self.path_to_dict = path.join(dir_path, 'data/dict-chris6.ser.gz')
+        self.path_to_sihan_corpora_dict = path.join(dir_path, 'data/')
+        self.seg = StanfordSegmenter(
+            path_to_jar=self.path_to_jar,
+            java_class='edu.stanford.nlp.ie.crf.CRFClassifier',
+            path_to_model=self.path_to_model,
+            path_to_dict=self.path_to_dict,
+            path_to_sihan_corpora_dict=self.path_to_sihan_corpora_dict)
+
+    def segment_file(self, origin_file):
+        """segment a text file and return array of tokens"""
+        seg_result = self.seg.segment_file(origin_file)
+
+        translator = str.maketrans('', '', string.digits)
+        seg_result = seg_result.translate(translator)
+
+        seg_result = re.sub("[\.\!\/_,$%^*(+\"\']+|[+—！，：；。？、~@#￥%…&*（）]+", "", seg_result)
+        print(seg_result)
+
+        return seg_result.split()
