a7dabd8694f4e2f1b4fb58770c5ea36c030ba340
zhenxun815 <guoyiheng89@gmail.com>
2019-07-01 19:12:16 +0800

    add stanford test

---
+++ src/test_stanford/test_stanford_segmenter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# @Description: 
+# @File: test_stanford_segmenter.py.py
+# @Project: test_nlp
+# @Author: Yiheng
+# @Email: GuoYiheng89@gmail.com
+# @Time: 7/1/2019 14:48
+
+from src.stanford.worker import StanfordWorker
+
+
+def test_stanford_segmenter():
+    worker = StanfordWorker()
+    tokens = worker.segment_file('../resources/CN105253527A')
+    for token in tokens:
+        print(token)
+
+
+test_stanford_segmenter()
