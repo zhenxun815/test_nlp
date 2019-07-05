514ffa8858d1c176ac3f8ff77cb0e6ff85e5b903
zhenxun815 <guoyiheng89@gmail.com>
2019-07-05 17:30:11 +0800

    update note

--- src/test_gensim/corpus/test_tf_idf.py
+++ src/test_gensim/corpus/test_tf_idf.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
             '风 脱水 架 和 大头菜 风 脱水 网袋 所述 的 大头菜 风 脱水 架 主要 包括 底座 支柱 横架 横向',
             '连接 承重杆 所述 的 底座 通过 中间 的 多边形 孔 与 支柱 的 下端 的 多边形 柱 配合 而 固定']
 
-    tokenized_docs = [simple_preprocess(doc, min_len=1) for doc in docs]
+    tokenized_docs = [simple_preprocess(doc, min_len=2) for doc in docs]
 
     my_dct = Dictionary(tokenized_docs)
     print('dictionary is {}'.format(my_dct.token2id))
